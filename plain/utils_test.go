edaa863ba27536ee846855a4fe1b34083335186a
Manuel Carmona <manu.carmona90@gmail.com>
2019-06-20 11:03:03 +0100

    plain: add IsFirstRepositoryBare function
    Signed-off-by: Manuel Carmona <manu.carmona90@gmail.com>

--- plain/utils_test.go
+++ plain/utils_test.go
@@ -10,8 +10,47 @@ import (
 	"gopkg.in/src-d/go-billy.v4"
 	"gopkg.in/src-d/go-billy.v4/memfs"
 	"gopkg.in/src-d/go-billy.v4/osfs"
-	"gopkg.in/src-d/go-git-fixtures.v3"
+	"gopkg.in/src-d/go-billy.v4/util"
+	fixtures "gopkg.in/src-d/go-git-fixtures.v3"
 )
+
+func TestIsFirstRepositoryBare(t *testing.T) {
+	var req = require.New(t)
+	req.True(true)
+
+	fs := memfs.New()
+
+	_, err := IsFirstRepositoryBare(fs, "/")
+	req.True(ErrRepositoriesNotFound.Is(err))
+
+	req.NoError(fs.MkdirAll("/foo/.git", 0755))
+	createValidDotGit(req, fs, "/foo/.git")
+	ok, err := IsFirstRepositoryBare(fs, "/")
+	req.NoError(err)
+	req.False(ok)
+
+	req.NoError(util.RemoveAll(fs, "/foo"))
+
+	req.NoError(fs.MkdirAll("/bare", 0755))
+	createValidDotGit(req, fs, "/bare")
+	ok, err = IsFirstRepositoryBare(fs, "/")
+	req.NoError(err)
+	req.True(ok)
+
+	req.NoError(util.RemoveAll(fs, "/bare"))
+
+	req.NoError(fs.MkdirAll("/aa/bare", 0755))
+	createValidDotGit(req, fs, "/aa/bare")
+
+	req.NoError(fs.MkdirAll("/aa/bb/foo.git", 0755))
+	createValidDotGit(req, fs, "/aa/bb/foo.git")
+
+	ok, err = IsFirstRepositoryBare(fs, "/")
+	req.NoError(err)
+	req.True(ok)
+
+	req.NoError(util.RemoveAll(fs, "/"))
+}
 
 func TestIsRepository(t *testing.T) {
 	require := require.New(t)
